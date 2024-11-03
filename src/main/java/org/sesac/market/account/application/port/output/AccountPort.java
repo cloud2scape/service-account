3e86d4c42b3d78f0b60014907354a5942d1a1d14
Ok JaeOok <okjaeook98@gmail.com>
2024-11-03 16:35:01 +0900

    feat: account port 추가
    for persistence

---
+++ src/main/java/org/sesac/market/account/application/port/output/AccountPort.java
@@ -0,0 +1,23 @@
+package org.sesac.market.account.application.port.output;
+
+import org.sesac.market.account.domain.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface AccountPort {
+    Account save(Account account);
+
+    Account update(Account account);
+
+    void delete(Account account);
+
+    Account get(Account account);
+
+    Page<Account> getMultiple(Pageable pageable);
+
+    boolean exists(String email);
+
+    boolean exists(UUID id);
+}
