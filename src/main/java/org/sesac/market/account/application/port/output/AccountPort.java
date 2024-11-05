046084e9ac7f893739993706e98cc90131c454bf
Ok JaeOok <okjaeook98@gmail.com>
2024-11-05 22:19:26 +0900

    refactor: Transactional 위치 조정
    `#` entity 와 도메인 결합으로 인한 수행 위치 변경
    
    BREAKING-CHANGE: Transaction이 service에서 수행됨

--- src/main/java/org/sesac/market/account/application/port/output/AccountPort.java
+++ src/main/java/org/sesac/market/account/application/port/output/AccountPort.java
@@ -4,16 +4,15 @@ import org.sesac.market.account.domain.model.Account;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountPort {
     Account save(Account account);
 
-    Account update(Account account);
-
     void delete(Account account);
 
-    Account get(Account account);
+    Optional<Account> get(Account account);
 
     Page<Account> getMultiple(Pageable pageable);
 
