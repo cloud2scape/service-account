b1fa7f46d052ca6137e7be90018c45d311909e5c
Ok JaeOok <okjaeook98@gmail.com>
2024-11-05 23:50:19 +0900

    fix: 유효성 검사 옵션 변경

--- src/main/java/org/sesac/market/account/application/dto/request/UpdateAccountRequest.java
+++ src/main/java/org/sesac/market/account/application/dto/request/UpdateAccountRequest.java
@@ -1,5 +1,6 @@
 package org.sesac.market.account.application.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
@@ -8,6 +9,6 @@ import java.util.UUID;
 @Builder(toBuilder = true)
 public record UpdateAccountRequest(
         @NotNull UUID id,
-        @NotNull String name
+        @NotEmpty String name
 ) {
 }
